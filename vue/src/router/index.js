9a9298d0110642f35830692806ca17ede9b32130
shahidullah Piaro <shahidullah.piyaro@gmail.com>
2023-12-09 05:48:28 +0600

    project setup with laravel, vue and tailwind css

---
+++ vue/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from '../views/Dashboard.vue';
+import Register from '../views/Register.vue';
+import Login from '../views/Login.vue';
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ vue/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Login</h1>
+</template>
+
+<script>
+
+export default {
+    name: "Login"
+}
+
+</script>
+
+<style scoped>
+
+</style>
