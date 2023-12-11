7f82fe72ade041370afad62db0bcc305de33f552
shahidullah Piaro <shahidullah.piyaro@gmail.com>
2023-12-11 23:19:11 +0600

    implemented login on frontend and backend

---
+++ vue/src/components/Alert.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+    <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+      {{ errorMsg }}
+      <span @click="errorMsg = ''"
+        class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -14,11 +14,21 @@
     </p>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" @click="login">
+            <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+                {{ errorMsg }}
+                <span @click="errorMsg = ''"
+                    class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
             <div>
                 <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email"
+                    <input id="email" name="email" type="email" autocomplete="email" v-model="user.email"
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
             </div>
@@ -29,7 +39,18 @@
                 </div>
                 <div class="mt-2">
                     <input id="password" name="password" type="password" autocomplete="current-password"
+                        v-model="user.password"
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                    <input id="remember-me" name="remember-me" type="checkbox" v-model="user.remember"
+                        class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+                    <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                        Remember me
+                    </label>
                 </div>
             </div>
 
@@ -42,10 +63,33 @@
     </div>
 </template>
 
-<script>
+<script setup>
+import store from '../store';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
-export default {
-    name: "Login"
+const router = useRouter();
+const user = {
+    email: '',
+    password: '',
+    remember: false
+}
+
+let errorMsg = ref("");
+
+async function login(ev) {
+    ev.preventDefault();
+
+    try {
+        await store.dispatch('login', user);
+        router.push({ name: 'Dashboard' });
+    } catch (error) {
+        if (error.response.data.error) {
+            errorMsg.value = error.response.data.error;
+        } else {
+            errorMsg.value = "An unknown error occurred.";
+        }
+    }
 }
 
 </script>
